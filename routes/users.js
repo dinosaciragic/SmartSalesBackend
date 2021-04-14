dda736f59cafb4d0f434cc78a8b4d1026f735376
Dino Saciragic <dino@appit.ba>
2021-04-14 14:10:02 +0200

    Save

--- routes/users.js
+++ routes/users.js
@@ -152,7 +152,10 @@ router.post('/login', async (req, res) => {
 router.put('/edit/:id', upload.single('companyImage'), async (req, res, next) => {
 
     var companyImage;
-    /*  companyImage = req.file.path; add later  */
+
+    if (req.file) {
+        companyImage = req.file.path;
+    }
 
     const {
         _id,
