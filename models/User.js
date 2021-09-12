160609253b19b7f7e9418191933eb366e8358490
Dino Saciragic <dino@appit.ba>
2021-09-12 18:57:48 +0200

    save

--- models/User.js
+++ models/User.js
@@ -1,54 +1,51 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    savePassAsPlainText: {
-        type: String,
-        required: true
-    },
-    companyType: {
-        type: String
-    },
-    companyName: {
-        type: String
-    },
-    companyName: {
-        type: String
-    },
-    description: {
-        type: String,
-    },
-    contactNumber: {
-        type: String
-    },
-    companyImage: {
-        type: String
-    },
-    website: {
-        type: String
-    },
-    instagram: {
-        type: String
-    },
-    facebook: {
-        type: String
-    },
-    followerIds: {
-        type: Array
-    },
-    location: {
-        type: String
-    },
-    postsLeft: {
-        type: Number
-    }
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  savePassAsPlainText: {
+    type: String,
+    required: true,
+  },
+  companyType: {
+    type: String,
+  },
+  companyName: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  contactNumber: {
+    type: String,
+  },
+  companyImage: {
+    type: String,
+  },
+  website: {
+    type: String,
+  },
+  instagram: {
+    type: String,
+  },
+  facebook: {
+    type: String,
+  },
+  followerIds: {
+    type: Array,
+  },
+  location: {
+    type: String,
+  },
+  postsLeft: {
+    type: Number,
+  },
 });
 
 const User = mongoose.model('User', UserSchema);
